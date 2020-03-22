001e879e24fbc0886692c033dcbc46cb8d8c6410
Pascal <Mineformatiker@bitbucket.org>
2020-03-22 15:40:31 +0100

    Name des Tisch wird angezeigt

---
+++ frontend/lib/tisch.js
@@ -0,0 +1,15 @@
+var backendBase = "https://digitalbar.newhouse.de/backend";
+
+function displayName(){
+   function reqListener () {
+        var room = JSON.parse(this.responseText)
+        document.getElementById('table').innerHTML("Tisch " + room.name)
+   }
+
+   var urlParams = new URLSearchParams(window.location.search);
+   var id = urlParams.get('roomId');
+   var oReq = new XMLHttpRequest();
+   oReq.addEventListener("load", reqListener);
+   oReq.open("GET", backendBase + "/rooms/" + id);
+   oReq.send();
+}
