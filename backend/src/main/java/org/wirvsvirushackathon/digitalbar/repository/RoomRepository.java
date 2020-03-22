68c987c909a5c84ab4197c1facea2baeadd47682
Dennis Neuhaus <dennis@newhouse.de>
2020-03-22 20:54:52 +0100

    remove backend from master

--- backend/src/main/java/org/wirvsvirushackathon/digitalbar/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package org.wirvsvirushackathon.digitalbar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.wirvsvirushackathon.digitalbar.domain.Room;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
