8a64f309f0bcbb6668dab20287a8256077e21d3c
Pascal <Mineformatiker@bitbucket.org>
2020-03-22 13:09:06 +0100

    Auf in den Abgrund! (Debugging)

--- frontend/lib/barliste.js
+++ frontend/lib/barliste.js
@@ -18,7 +18,7 @@ function createBars(filter, tags){
        tags: ["Spiele","Videochat"]
    }]*/
    function reqListener () {
-        console.log(this.responseText)
+        console.log(this)
          var bars = JSON.parse(this.responseText)
         document.getElementById("searchbar").addEventListener("input", (event) => {
             if(event.target.value != filter)
