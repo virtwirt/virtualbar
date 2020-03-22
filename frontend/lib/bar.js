3b6e464560f6720d6f37535711b76768c4b76fb5
Jeremias Lemm <jeremias.lemm@rwth-aachen.de>
2020-03-22 13:30:36 +0100

    merge

--- frontend/lib/bar.js
+++ frontend/lib/bar.js
@@ -1,10 +1,17 @@
 var backendBase = "https://digitalbar.newhouse.de/backend";
 
+function navigateToRoom(roomId) {
+  return function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var id = urlParams.get('barId');
+    window.location.href = 'tisch.html?barId=' + id + '&roomId=' + roomId;
+  }
+}
+
 function createRooms(){
     // get rooms
     function reqListener () {
-      console.log(this)
-      var rooms = JSON.parse(this.responseText);
+      var rooms = JSON.parse(this.responseText).rooms;
       for(var i = 0; i < rooms.length; i++) {
           var room = rooms[i];
 
@@ -18,13 +25,12 @@ function createRooms(){
           var table = document.createElement("img")
           table.setAttribute("class", "ui image")
           table.setAttribute("src", "table.jpg")
-          table.setAttribute("onclick", "window.location.href = 'tisch.html'")
-          createTable(rooms[i].name)
+          createTable(rooms[i])
 
           // Sitzplätze
           // vom Backend Sitzplätze für entsprechenden Raum holen
           //var count = room.maxSeats;
-          var count = 4
+          var count = rooms[i].occupiedSeats
           for(var j = 0; j < count; j++) {
               var seat = document.createElement("img")
               seat.setAttribute("class", "ui avatar image")
@@ -34,65 +40,69 @@ function createRooms(){
       }
     }
 
+    var urlParams = new URLSearchParams(window.location.search);
+    var id = urlParams.get('barId');
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", reqListener);
-    oReq.open("GET", backendBase + "/rooms");
+    oReq.open("GET", backendBase + "/bars/" + id);
     oReq.send();
 
 }
 
-function createTable(containerEle){
-	
+function createTable(room){
+
 	var card = document.createElement("div")
-    card.setAttribute("class", "card")
-	
+  card.setAttribute("class", "card")
+
 	var blurCntnr = document.createElement("div")
-    blurCntnr.setAttribute("class", "blurring dimmable image")
-	
+  blurCntnr.setAttribute("class", "blurring dimmable image")
+
 	var dimmer = document.createElement("div")
-    dimmer.setAttribute("class", "ui dimmer")
-	
+  dimmer.setAttribute("class", "ui dimmer")
+
 	var cntnt = document.createElement("div")
-    cntnt.setAttribute("class", "content")
-	
+  cntnt.setAttribute("class", "content")
+
 	var center = document.createElement("div")
-    center.setAttribute("class", "center")
-	
-	var room = document.createElement("div")
-    center.setAttribute("class", "header")
-	
+  center.setAttribute("class", "center")
+
+	var roomElem = document.createElement("div")
+  center.setAttribute("class", "header")
+
 	var bla = document.createElement("div")
-    bla.setAttribute("class", "ui inverted button")
+  bla.setAttribute("class", "ui inverted button")
 	bla.innerHTML = "An der Unterhaltung teilnehmen"
-	bla.setAttribute("onclick", "window.location.href = 'tisch.html'")
+	bla.addEventListener("click", navigateToRoom(room.id))
 	var table = document.createElement("img")
 	table.setAttribute("class", "ui medium image")
-    table.setAttribute("src", "table.png")
-	
+  table.setAttribute("src", "table.png")
+
 	card.appendChild(blurCntnr)
 	blurCntnr.appendChild(dimmer)
 	blurCntnr.appendChild(table)
 	dimmer.appendChild(cntnt)
 	cntnt.appendChild(center)
-	center.appendChild(room)
+	center.appendChild(roomElem)
 	center.appendChild(bla)
 	createTags(center)
-	document.getElementById(containerEle).appendChild(card)
-	
+	document.getElementById(room.name).appendChild(card)
+
 }
 
 function createTags(container){
 	var labelContainer = document.createElement("div")
-    labelContainer.setAttribute("class", "ui red labels")
+  labelContainer.setAttribute("class", "ui red labels")
 
 	var labelse = ['Chat', 'Voice', 'Trinkspiele']
-	
+
 	for(var i = 0; i < labelse.length; i++) {
 		var tag = document.createElement("a")
 		tag.setAttribute("class", "ui label")
 		tag.innerHTML = labelse[i]
-		
+
 		labelContainer.appendChild(tag)
 	}
 	container.appendChild(labelContainer)
 }
+
+createRooms();
